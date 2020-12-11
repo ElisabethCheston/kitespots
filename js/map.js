105839e0f233cd00a9b75f1c4400d718a51cb302
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-11 14:21:47 +0000

    Add text window for the markers with name.

--- js/map.js
+++ js/map.js
@@ -61,11 +61,38 @@ function initialize() {
             {coords: {lat: 61.2406976, lng: 17.2613011 },  content:'ORSUNDET', windDirection: 'S/SW'},        // Orsundet SSW NNE//
             {coords: {lat: 61.2352256, lng: 17.2766953 },  content:'ENSKÄR', windDirection: 'S/SE'},          // Enskär SSE //
             ];
-            
+
                 //  Loop through markers
         for (var i = 0; i < markers.length;i++){
         //  Add marker      
             addMarker(markers[i]);
         }
+           
+    //  Add marker function
+        function addMarker(props){
+            var marker = new google.maps.Marker({
+                position:props.coords,
+                map:map,
+                
+    //  icon: props.iconImage
+        });
+
+    //  Check for customicon
+        if(props.iconImage){
+        //Set icon image
+        marker.setIcon(props.iconImage);
+        }
+
+    //  Check content
+        if (props.content){
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+        });
+
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        });
+        }
+    }
 
 }
